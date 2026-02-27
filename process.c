d463d4fc265d48cbc4b56b39f80473f1a1650a60
Abdulselam Adill <forcoding39@gmail.com>
2026-02-27 10:02:18 -0500

    Add: documentation for argument parser

--- process.c
+++ process.c
@@ -3,8 +3,38 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// Parser 
+/* The parser will extract the following properties
+ * --per-process
+ * --systemWide
+ * --Vnodes
+ * --composite
+ * --summary
+ * --threshold=X
+ * 
+ * 
+ * DEFAULT BEHAVIOR: if no argument is passed to 
+ * the program, you must implement one of the 
+ * following approaches. 
+ * 
+ * 1. the program will display the composite table, 
+ * i.e. same effect as having used the --composite 
+ * flag
+ * 
+ * 2. the program will display all the tables, i.e. 
+ * same effect as having used all the flagged 
+ * arguments  --per-process --systemWide --Vnode 
+ * --composite 
+ */
+
+
 int main(int argc, char** argv){
+    // First step, create a parser for the 
+    //passed in arguments; note: take from 
+    // assignment 1
+ 
     
+
 
     return 0;
 }
