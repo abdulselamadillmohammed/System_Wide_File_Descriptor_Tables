861194078ba1e44c248bc3d908b7fee1e0c8d15b
Abdulselam Adill <forcoding39@gmail.com>
2026-02-27 10:08:54 -0500

    add parser boilerplate

--- process.c
+++ process.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <string.h>
+
 // Parser 
 /* The parser will extract the following properties
  * --per-process
@@ -25,6 +27,14 @@
  * same effect as having used all the flagged 
  * arguments  --per-process --systemWide --Vnode 
  * --composite 
+ * 
+ * Positional argument: 
+ *  My role is only to accept on positional argument 
+ * which indicates that a particular process id number
+ * (PID), if not specified; the program will attempt 
+ * to process all the currently running processes for the
+ * user executing the program
+ * 
  */
 
 
@@ -32,9 +42,11 @@ int main(int argc, char** argv){
     // First step, create a parser for the 
     //passed in arguments; note: take from 
     // assignment 1
- 
-    
 
+    // Print out the args
+    printf("%s\n", argv[0]);
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
 
     return 0;
 }
---
+++ template.py
@@ -0,0 +1,15 @@
+
+## this will take in postional arguments
+def parser(argvv):
+
+
+    return 
+
+
+# Argv acts as a list of values which I must extract from via positons
+# ./process
+#--per-process
+#--systemWide
+
+
+argv = ["", "","","","",""]
