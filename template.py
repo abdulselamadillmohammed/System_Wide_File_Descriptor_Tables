cc114c092fa211cc949916461748e4ecc37b813d
Abdulselam Adill <forcoding39@gmail.com>
2026-02-27 10:16:21 -0500

    finish python templating for parser

--- template.py
+++ template.py
@@ -1,9 +1,32 @@
 
 ## this will take in postional arguments
-def parser(argvv):
 
 
-    return 
+# Define in the background what these taskes do:
+"""
+Ie, write in function documentation
+"""
+argv = ["--per-process", 
+        "--systemWide",
+        "--Vnodes",
+        "--composite",
+        "--summary",
+
+        # On threshold check if it is a number
+        "--threshold=X"
+        ]
+argc = len(argv)
+
+# Easier, hold something for flagged 
+# another for positonal
+set_arguments = [0] * argc
+
+def parser(argc: int, argv: str) -> None:
+    for i in range(argc):
+        set
+
+
+parser(argc, argv)
 
 
 # Argv acts as a list of values which I must extract from via positons
@@ -11,5 +34,7 @@ def parser(argvv):
 #--per-process
 #--systemWide
 
-
-argv = ["", "","","","",""]
+def main():
+    parser(argc, argv)
+    # if the flagged aguments sum to 0, then set them to which
+    # ever default version you want
