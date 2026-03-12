d7cf3d06510291dfec4251b3d9d74ae7d66e7789
Abdulselam Adill <forcoding39@gmail.com>
2026-03-12 15:58:51 -0400

    add: append to file descriptor table function

--- table.c
+++ table.c
@@ -8,3 +8,33 @@ void init_fd_table(FDTable *table){
     table->capacity = 0;
 }
 
+void append_fd_entry(FDTable *table, FDEntry entry)
+{
+    if (table->size == table->capacity)
+    {
+        int new_capacity;
+
+        if (table->capacity == 0)
+            new_capacity = 10;
+        else
+            new_capacity = table->capacity * 2;
+
+        FDEntry *new_entries = malloc(new_capacity * sizeof(FDEntry));
+
+        if (new_entries == NULL)
+        {
+            fprintf(stderr, "Memory allocation failed\n");
+            exit(1);
+        }
+
+        for (int i = 0; i < table->size; i++)
+            new_entries[i] = table->entries[i];
+
+        free(table->entries);
+        table->entries = new_entries;
+        table->capacity = new_capacity;
+    }
+
+    table->entries[table->size] = entry;
+    table->size++;
+}
