a79f048e2a5cb3845a26ad63da17deda8ac210ad
Abdulselam Adill <forcoding39@gmail.com>
2026-03-12 16:26:09 -0400

    add: file descriptor collecor function initalization

---
+++ proc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "proc.h"
+#include "table.h"
+
+void collect_fd_for_pid(pid_t pid, FDTable *table){
+    
+}
